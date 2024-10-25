871aebd4411a84b48c7668ff4383401692d3c7fc
samo-codehouse <soh@codehousegroup.com>
2024-10-25 15:40:15 +1100

    updating manifest and offline page

--- html/service-worker.js
+++ html/service-worker.js
@@ -1,24 +1,27 @@
 // https://learn.microsoft.com/en-us/microsoft-edge/progressive-web-apps-chromium/how-to/service-workers
 // The name of the cache your app uses.
-const CACHE_NAME = "my-app-cache";
+const cacheName = 'actewagl-pwa-cache';
 // The list of static files your app needs to start.
 // Note the offline page in this list.
-const PRE_CACHED_RESOURCES = ["/offline"];
+const cachedResources = [
+    '/offline.html'
+];
 
-// Listen to the `install` event.
-self.addEventListener("install", event => {
-    self.skipWaiting();
+// install event
+self.addEventListener('install', event => {
+    // self.skipWaiting();
     async function preCacheResources() {
         // Open the app's cache.
-        const cache = await caches.open(CACHE_NAME);
+        const cache = await caches.open(cacheName);
         // Cache all static resources.
-        cache.addAll(PRE_CACHED_RESOURCES);
+        cache.addAll(cachedResources);
     }
     event.waitUntil(preCacheResources());
-    console.log('install event');
+    console.info('install event');
 });
 
-self.addEventListener("fetch", event => {
+// fetch event
+self.addEventListener('fetch', event => {
     async function navigateOrDisplayOfflinePage() {
         try {
             // Try to load the page from the network.
@@ -26,18 +29,14 @@ self.addEventListener("fetch", event => {
             return networkResponse;
         } catch (error) {
             // The network call failed, the device is offline.
-            const cache = await caches.open(CACHE_NAME);
-            const cachedResponse = await cache.match("/offline");
+            const cache = await caches.open(cacheName);
+            const cachedResponse = await cache.match('/offline.html');
             return cachedResponse;
         }
     }
-    console.log('fetch event');
-    console.log(event);
-
-    // Only call event.respondWith() if this is a navigation request
-    // for an HTML page.
-    if (event.request.mode === 'navigate' || event.request.mode === 'cors') {
-        console.log('navigate');
+    if (event.request.mode === 'navigate') {
+        console.log(event.request.mode);
         event.respondWith(navigateOrDisplayOfflinePage());
     }
+    console.info('fetch event');
 });
