700323b515aa5296936a3181b58bdfcad8cf1f6d
samo-codehouse <soh@codehousegroup.com>
2024-11-12 09:00:53 +1100

    styling updates

--- html/service-worker.js
+++ html/service-worker.js
@@ -25,21 +25,18 @@ if (workbox.navigationPreload.isSupported()) {
 self.addEventListener('fetch', (event) => {
     if (event.request.mode === 'navigate') {
         event.respondWith((async () => {
-        try {
-            const preloadResponse = await event.preloadResponse;
-
-            if (preloadResponse) {
-                return preloadResponse;
+            try {
+                const preloadResponse = await event.preloadResponse;
+                if (preloadResponse) {
+                    return preloadResponse;
+                }
+                const networkResponse = await fetch(event.request);
+                return networkResponse;
+            } catch (error) {
+                const cache = await caches.open(appCache);
+                const cachedResponse = await cache.match(offlineFallback);
+                return cachedResponse;
             }
-
-            const networkResponse = await fetch(event.request);
-            return networkResponse;
-        } catch (error) {
-
-            const cache = await caches.open(appCache);
-            const cachedResponse = await cache.match(offlineFallback);
-            return cachedResponse;
-        }
         })());
     }
 });
